b8caeae2a20f2b0dafd675eaed13c72398c98b96
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 11:26:36 +0100

    fix(deploy): try making both dev and deploy work

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/quizz-dessins-animes",
-  // basePath: process.env.NODE_ENV === "production" ? "/quizz-dessins-animes" : "",
+  // output: "export",
+  // basePath: "/quizz-dessins-animes",
+  basePath: process.env.NODE_ENV === "production" ? "/quizz-dessins-animes" : "",
 };
 
 export default nextConfig;
