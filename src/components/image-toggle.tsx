92b0aacd963f78a5ee03a01ae5fe2120cc664e65
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 10:46:48 +0100

    feat(quizz): fix button labels

--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -3,8 +3,8 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const easyHint = "/assets/images/dragon-ball-01.png";
-const hardHint = "/assets/images/dragon-ball-02.jpg";
+const hardHint = "/assets/images/dragon-ball-01.png";
+const easyHint = "/assets/images/dragon-ball-02.jpg";
 
 export function ImageToggle() {
   const [imageSrc, setImageSrc] = useState(easyHint);
@@ -13,11 +13,11 @@ export function ImageToggle() {
     <div className="grid gap-4 place-items-center">
       <Image src={imageSrc} width={300} height={300} alt="hint image" />
       <div className="flex gap-4">
-        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(easyHint)}>
-          Indice facile
-        </button>
         <button className="p-4 border border-gray-200" onClick={() => setImageSrc(hardHint)}>
           Indice difficile
+        </button>
+        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(easyHint)}>
+          Indice facile
         </button>
       </div>
     </div>
