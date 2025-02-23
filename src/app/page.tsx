71698409144c15e41134a0fcb4a36dcc5933eb1e
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 12:29:18 +0100

    feat(quizz): use data from file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { ImageToggle } from "@/components/image-toggle";
+import { Anime } from "@/types";
+import data from "../data/dessins-animes.json";
 
 export default function Home() {
+  const animes = data as Anime[];
+
   return (
     <div className="outline outline-sky-400">
-      <ImageToggle />
+      <ImageToggle anime={animes[0]} />
     </div>
   );
 }
--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -1,24 +1,23 @@
 "use client";
 
+import { Anime } from "@/types";
 import Image from "next/image";
 import { useState } from "react";
 
 const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
 
-const hardHint = PREFIX + "assets/images/dragon-ball-01.png";
-const easyHint = PREFIX + "assets/images/dragon-ball-02.jpg";
-
-export function ImageToggle() {
-  const [imageSrc, setImageSrc] = useState(hardHint);
+export function ImageToggle({ anime }: { anime: Anime }) {
+  console.log(anime);
+  const [imageSrc, setImageSrc] = useState(PREFIX + anime.hardHint);
 
   return (
     <div className="grid gap-4 place-items-center">
       <Image src={imageSrc} width={300} height={300} alt="hint image" />
       <div className="flex gap-4">
-        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(hardHint)}>
+        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(PREFIX + anime.hardHint)}>
           Indice difficile
         </button>
-        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(easyHint)}>
+        <button className="p-4 border border-gray-200" onClick={() => setImageSrc(PREFIX + anime.easyHint)}>
           Indice facile
         </button>
       </div>
