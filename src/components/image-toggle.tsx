aad6a160b5db5352f772cc47cacdc9a67e48225b
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 11:45:35 +0100

    fix(deploy): revert last commit

--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -3,12 +3,10 @@
 import Image from "next/image";
 import { useState } from "react";
 
-// const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
+const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
 
-// const hardHint = PREFIX + "assets/images/dragon-ball-01.png";
-// const easyHint = PREFIX + "assets/images/dragon-ball-02.jpg";
-const hardHint = process.env.NEXT_PUBLIC_IMAGE_SRC_PATH_PREFIX + "assets/images/dragon-ball-01.png";
-const easyHint = process.env.NEXT_PUBLIC_IMAGE_SRC_PATH_PREFIX + "assets/images/dragon-ball-02.jpg";
+const hardHint = PREFIX + "assets/images/dragon-ball-01.png";
+const easyHint = PREFIX + "assets/images/dragon-ball-02.jpg";
 
 export function ImageToggle() {
   const [imageSrc, setImageSrc] = useState(hardHint);
