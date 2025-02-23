5255c54799aba86d6f2ca219c2e0516ab11daff8
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 10:47:09 +0100

    feat(deploy): set base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NODE_ENV === "production" ? "/quizz-dessins-animes/" : "",
 };
 
 export default nextConfig;
