5470c7993689d8207523a05c9614c3bc366cd1bb
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 11:18:49 +0100

    fix(deploy): remove '/' at start of image path

--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -3,15 +3,15 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const hardHint = "/assets/images/dragon-ball-01.png";
-const easyHint = "/assets/images/dragon-ball-02.jpg";
+const hardHint = "assets/images/dragon-ball-01.png";
+const easyHint = "assets/images/dragon-ball-02.jpg";
 
 export function ImageToggle() {
   const [imageSrc, setImageSrc] = useState(easyHint);
 
   return (
     <div className="grid gap-4 place-items-center">
-      <Image src={imageSrc} width={300} height={300} alt="hint image" unoptimized />
+      <Image src={imageSrc} width={300} height={300} alt="hint image" />
       <div className="flex gap-4">
         <button className="p-4 border border-gray-200" onClick={() => setImageSrc(hardHint)}>
           Indice difficile
