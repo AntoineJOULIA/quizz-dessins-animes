7d2e5b815650299f932425719a4f87c0df6b5ca4
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 11:41:34 +0100

    feat(deploy): images prefix as environment variables

--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -3,13 +3,15 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
+// const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
 
-const hardHint = PREFIX + "assets/images/dragon-ball-01.png";
-const easyHint = PREFIX + "assets/images/dragon-ball-02.jpg";
+// const hardHint = PREFIX + "assets/images/dragon-ball-01.png";
+// const easyHint = PREFIX + "assets/images/dragon-ball-02.jpg";
+const hardHint = process.env.NEXT_PUBLIC_IMAGE_SRC_PATH_PREFIX + "assets/images/dragon-ball-01.png";
+const easyHint = process.env.NEXT_PUBLIC_IMAGE_SRC_PATH_PREFIX + "assets/images/dragon-ball-02.jpg";
 
 export function ImageToggle() {
-  const [imageSrc, setImageSrc] = useState(easyHint);
+  const [imageSrc, setImageSrc] = useState(hardHint);
 
   return (
     <div className="grid gap-4 place-items-center">
