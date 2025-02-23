4749ba840fed37d3edb8296d15b0b04da33e42c4
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-23 10:56:19 +0100

    fix(deploy): update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   basePath: "/quizz-dessins-animes",
   // basePath: process.env.NODE_ENV === "production" ? "/quizz-dessins-animes/" : "",
 };
