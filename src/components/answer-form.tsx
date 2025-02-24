e219d83ce16fababf3e5acdbe713e9497d3ecc3f
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-24 16:34:29 +0100

    feat(quizz): show message on submit and title if correct

--- src/components/answer-form.tsx
+++ src/components/answer-form.tsx
@@ -8,48 +8,60 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { Anime } from "@/types";
 import { checkAnswer } from "@/lib/utils";
+import { useState } from "react";
 
 const formSchema = z.object({
   answer: z.string().min(1),
 });
 
 export default function AnswerForm({ anime }: { anime: Anime }) {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    values: { answer: "" },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     const success = checkAnswer(anime, values.answer);
     if (success) {
-      alert("Bonne réponse !");
+      setIsSuccess(true);
+      setMessage("Bonne réponse !");
     } else {
-      alert("Mauvaise réponse !");
+      setIsSuccess(false);
+      setMessage("Mauvaise réponse !");
     }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
-        <FormField
-          control={form.control}
-          name="answer"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Réponse</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Ma réponse"
-                  type="string"
-                  {...field}
-                  onChange={(event) => field.onChange(event.target.value)}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Soumettre</Button>
-      </form>
-    </Form>
+    <div className="grid gap-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+          <FormField
+            control={form.control}
+            name="answer"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Réponse</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ma réponse"
+                    type="string"
+                    {...field}
+                    onChange={(event) => field.onChange(event.target.value)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Soumettre</Button>
+        </form>
+      </Form>
+
+      {message && <p className="text-2xl font-bold">{message}</p>}
+      {isSuccess && <p className="text-2xl font-bold">{anime.title}</p>}
+    </div>
   );
 }
