3bd839e5812adb5c13708f94a2285dceb789c3dd
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-25 16:50:27 +0100

    refactor(quizz): apply useAnswer to AnswersBoard

--- src/components/answers-board.tsx
+++ src/components/answers-board.tsx
@@ -1,18 +1,11 @@
 "use client";
 
+import { useAnswers } from "@/hooks/useAnswers";
 import { Anime } from "@/types";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export function AnswerBoard({ animes }: { animes: Anime[] }) {
-  const [foundAnimeIds, setFoundAnimeIds] = useState<string[]>([]);
-
-  useEffect(() => {
-    const storedAnimeIds = window.localStorage.getItem("quizz-anime");
-    if (storedAnimeIds) {
-      setFoundAnimeIds(storedAnimeIds.split(","));
-    }
-  }, []);
+  const { correctAnswers } = useAnswers();
 
   return (
     <div className="grid gap-4">
@@ -20,7 +13,7 @@ export function AnswerBoard({ animes }: { animes: Anime[] }) {
         <div key={anime.id} className="bg-white p-4 rounded-lg shadow-md">
           <Link href={`/${anime.id}`} className="flex gap-4">
             <h2 className="text-xl font-semibold">{anime.id}</h2>
-            <h2 className="text-xl font-semibold">{foundAnimeIds.includes(anime.id) ? anime.title : ""}</h2>
+            <h2 className="text-xl font-semibold">{correctAnswers.includes(anime.id) ? anime.title : ""}</h2>
           </Link>
         </div>
       ))}
