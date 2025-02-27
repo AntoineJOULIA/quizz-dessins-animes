dcc0a3410da602f471636c08db9093b31a7d9420
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-27 11:41:24 +0100

    ui(quizz): indicator on hint type

--- src/components/image-toggle.tsx
+++ src/components/image-toggle.tsx
@@ -3,21 +3,37 @@
 import { Anime } from "@/types";
 import Image from "next/image";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 
 const PREFIX = process.env.NODE_ENV !== "production" ? "/" : "";
 
 export function ImageToggle({ anime }: { anime: Anime }) {
-  const [imageSrc, setImageSrc] = useState(PREFIX + anime.hardHint);
+  const [hintType, setHintType] = useState<"easy" | "hard">("hard");
 
   return (
     <div className="grid gap-4 place-items-center">
-      <Image src={imageSrc} width={800} height={600} alt="hint image" />
+      <Image
+        className="aspect-4/3 object-cover w-full h-[600px]"
+        src={hintType === "easy" ? PREFIX + anime.easyHint : PREFIX + anime.hardHint}
+        width={800}
+        height={600}
+        alt="hint image"
+      />
       <div className="flex gap-4">
-        <Button variant={"secondary"} onClick={() => setImageSrc(PREFIX + anime.hardHint)}>
+        <Button
+          className="text-xl px-8 py-6"
+          size={"lg"}
+          variant={hintType === "hard" ? "outline" : "secondary"}
+          onClick={() => setHintType("hard")}
+        >
           Indice difficile
         </Button>
-        <Button variant={"secondary"} onClick={() => setImageSrc(PREFIX + anime.easyHint)}>
+        <Button
+          className="text-xl px-8 py-6"
+          size={"lg"}
+          variant={hintType === "easy" ? "outline" : "secondary"}
+          onClick={() => setHintType("easy")}
+        >
           Indice facile
         </Button>
       </div>
