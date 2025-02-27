84a0c9570f73a97b8dbd38a6ae87f99d92364275
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-27 14:25:00 +0100

    ui(results): update board ui

--- src/components/answers-board.tsx
+++ src/components/answers-board.tsx
@@ -2,21 +2,41 @@
 
 import { useAnswers } from "@/hooks/useAnswers";
 import { Anime } from "@/types";
+import { Minus, Trophy } from "lucide-react";
 import Link from "next/link";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 export function AnswerBoard({ animes }: { animes: Anime[] }) {
   const { correctAnswers } = useAnswers();
 
   return (
-    <div className="grid gap-4">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-4 container mx-auto">
       {animes.map((anime) => (
-        <div key={anime.id} className="bg-white p-4 rounded-lg shadow-md">
-          <Link href={`/${anime.id}`} className="flex gap-4">
-            <h2 className="text-xl font-semibold">{anime.id}</h2>
-            <h2 className="text-xl font-semibold">{correctAnswers.includes(anime.id) ? anime.title : ""}</h2>
-          </Link>
-        </div>
+        <BoardItem key={anime.id} anime={anime} correctAnswers={correctAnswers} />
       ))}
     </div>
+  );
+}
+
+function BoardItem({ anime, correctAnswers }: { anime: Anime; correctAnswers: string[] }) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Link
+            href={`/${anime.id}`}
+            className="flex items-center gap-2 p-4 rounded-lg outline outline-gray-200 outline-1"
+          >
+            <p>{anime.id.padStart(3, "0")}</p>
+            {correctAnswers.includes(anime.id) ? (
+              <Trophy className="size-6 text-yellow-500" />
+            ) : (
+              <Minus className="size-4" />
+            )}
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>{correctAnswers.includes(anime.id) ? anime.title : "Pas encore trouvé !"}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
