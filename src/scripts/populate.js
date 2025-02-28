22244acb3f02fa17cbb076900e2707bde9439291
AntoineJOULIA <antoine.joulia@gmail.com>
2025-02-28 12:03:21 +0100

    feat(db): script to generate complete json from csv

--- src/scripts/populate.js
+++ src/scripts/populate.js
@@ -1,13 +1,43 @@
+// eslint-disable-next-line
 const fs = require("node:fs");
+// eslint-disable-next-line
 const Papa = require("papaparse");
 
-fs.readFile("../data/dessins-animes.csv", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+function shuffle(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  const result = Papa.parse(data, {
-    header: true,
-  });
-  console.log(result.data);
+  return shuffled;
+}
+
+let data = fs.readFileSync("../data/dessins-animes.csv", "utf8");
+let result = Papa.parse(data, {
+  header: true,
 });
+
+// Add hints, based on id
+const animesWithHints = result.data.map((anime) => {
+  return {
+    ...anime,
+    hardHint: `assets/images/${anime.id}-01.jpg`,
+    easyHint: `assets/images/${anime.id}-02.jpg`,
+  };
+});
+
+// Shuffle array
+const shuffledAnimes = shuffle(animesWithHints);
+
+// Add index property
+const animes = shuffledAnimes.map((item, index) => {
+  return {
+    ...item,
+    index: (index + 1).toString(),
+  };
+});
+
+fs.writeFileSync("../data/dessins-animes.json", JSON.stringify(animes), "utf8");
+// const sample = animes.slice(0, 10);
+// fs.writeFileSync("../data/dessins-animes_sample10.json", JSON.stringify(sample), "utf8");
+console.log("Conversion done");
