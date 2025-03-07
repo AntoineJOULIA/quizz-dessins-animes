76f2c7707d3929a28842fe0b53f8355668feb7f0
AntoineJOULIA <antoine.joulia@gmail.com>
2025-03-07 16:07:02 +0100

    feat(dragon-balls): dragon balls display

--- src/components/answers-board.tsx
+++ src/components/answers-board.tsx
@@ -14,7 +14,7 @@ export function AnswerBoard({ animes }: { animes: Anime[] }) {
   const totalCount = getAnimes().length;
 
   return (
-    <div className="container space-y-8 mx-auto">
+    <div className="container space-y-8 mx-auto relative">
       <Score statusList={animeStatus} totalCount={totalCount} />
       <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-4">
         {animes.map((anime) => (
@@ -41,7 +41,8 @@ function Score({
         Score actuel : <span className="text-6xl font-black">{correctCount}</span> / {totalCount}
       </p>
       <Image src="/assets/images/dragon-ball_radar.png" width={50} height={50} alt="Détecteur de dragon balls" />
-      <TrophyBar correctCount={correctCount} totalCount={totalCount} />
+      {/* <TrophyBar correctCount={correctCount} totalCount={totalCount} /> */}
+      <DragonBallCollection found={[1, 2, 4, 5, 7]} />
     </div>
   );
 }
@@ -74,6 +75,90 @@ function BoardItem({ anime, status }: { anime: Anime; status: Status }) {
   );
 }
 
+function DragonBallCollection({ found }: { found: number[] }) {
+  return (
+    <div className="grid grid-cols-[repeat(3,50px)] gap-2 place-items-center">
+      {found.includes(1) ? (
+        <Image
+          className="col-start-2 row-span-2"
+          src="/assets/images/dragon-ball_1-etoile.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 1 étoile"
+        />
+      ) : (
+        <EmptySlot className="col-start-2 row-span-2" />
+      )}
+      {found.includes(2) ? (
+        <Image
+          className="row-start-2 row-span-2"
+          src="/assets/images/dragon-ball_2-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 2 étoiles"
+        />
+      ) : (
+        <EmptySlot className="row-start-2 row-span-2" />
+      )}
+      {found.includes(3) ? (
+        <Image
+          className="col-start-3 row-start-2 row-span-2"
+          src="/assets/images/dragon-ball_3-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 3 étoiles"
+        />
+      ) : (
+        <EmptySlot className="col-start-3 row-start-2 row-span-2" />
+      )}
+      {found.includes(4) ? (
+        <Image
+          className="col-start-2 row-start-3 row-span-2"
+          src="/assets/images/dragon-ball_4-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 4 étoiles"
+        />
+      ) : (
+        <EmptySlot className="col-start-2 row-start-3 row-span-2" />
+      )}
+      {found.includes(5) ? (
+        <Image
+          className="col-start-1 row-start-4 row-span-2"
+          src="/assets/images/dragon-ball_5-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 5 étoiles"
+        />
+      ) : (
+        <EmptySlot className="col-start-1 row-start-4 row-span-2" />
+      )}
+      {found.includes(6) ? (
+        <Image
+          className="col-start-3 row-start-4 row-span-2"
+          src="/assets/images/dragon-ball_6-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 6 étoiles"
+        />
+      ) : (
+        <EmptySlot className="col-start-3 row-start-4 row-span-2" />
+      )}
+      {found.includes(7) ? (
+        <Image
+          className="col-start-2 row-start-5 row-span-2"
+          src="/assets/images/dragon-ball_7-etoiles.png"
+          width={50}
+          height={50}
+          alt="Dragon ball à 7 étoiles"
+        />
+      ) : (
+        <EmptySlot className="col-start-2 row-start-5 row-span-2" />
+      )}
+    </div>
+  );
+}
+
 // https://www.citypng.com/photo/27988/hd-png-dragon-ball-z-dbz-crystal-ball-4-stars
 function TrophyBar({ correctCount, totalCount }: { correctCount: number; totalCount: number }) {
   const currentScore = correctCount / totalCount;
@@ -90,38 +175,90 @@ function TrophyBar({ correctCount, totalCount }: { correctCount: number; totalCo
           />
         </div>
       ) : (
-        <div className="flex gap-4">
+        <div className="grid grid-cols-[repeat(3,50px)] gap-2">
           {/* {correctCount >= 5 && 
-            <Image src="/assets/images/dragon-ball_1-star.png" width={50} height={50} alt="Dragon ball à 1 étoile" />
+            <Image src="/assets/images/dragon-ball_1-etoile.png" width={50} height={50} alt="Dragon ball à 1 étoile" />
           }
       {correctCount >= 10 && 
-            <Image src="/assets/images/dragon-ball_2-stars.png" width={50} height={50} alt="Dragon ball à 2 étoiles" />
+            <Image src="/assets/images/dragon-ball_2-etoiles.png" width={50} height={50} alt="Dragon ball à 2 étoiles" />
       } */}
+
           {correctCount >= 1 && (
-            <Image src="/assets/images/dragon-ball_1-star.png" width={50} height={50} alt="Dragon ball à 1 étoile" />
+            <Image
+              className="col-start-2 row-span-2"
+              src="/assets/images/dragon-ball_1-etoile.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 1 étoile"
+            />
           )}
           {correctCount >= 2 && (
-            <Image src="/assets/images/dragon-ball_2-stars.png" width={50} height={50} alt="Dragon ball à 2 étoiles" />
+            <Image
+              className="row-start-2 row-span-2"
+              src="/assets/images/dragon-ball_2-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 2 étoiles"
+            />
           )}
           {currentScore >= 0.175 && (
-            <Image src="/assets/images/dragon-ball_3-stars.png" width={50} height={50} alt="Dragon ball à 3 étoiles" />
+            <Image
+              className="col-start-3 row-start-2 row-span-2"
+              src="/assets/images/dragon-ball_3-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 3 étoiles"
+            />
           )}
           {currentScore >= 0.3 && (
-            <Image src="/assets/images/dragon-ball_3-stars.png" width={50} height={50} alt="Dragon ball à 3 étoiles" />
-            // <Image src="/assets/images/dragon-ball_4-stars.png" width={50} height={50} alt="Dragon ball à 4 étoiles" />
+            <Image
+              className="col-start-2 row-start-3 row-span-2"
+              src="/assets/images/dragon-ball_4-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 3 étoiles"
+            />
           )}
           {currentScore >= 0.5 && (
-            <Image src="/assets/images/dragon-ball_5-stars.png" width={50} height={50} alt="Dragon ball à 5 étoiles" />
+            <Image
+              className="col-start-1 row-start-4 row-span-2"
+              src="/assets/images/dragon-ball_5-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 5 étoiles"
+            />
           )}
           {currentScore >= 0.75 && (
-            <Image src="/assets/images/dragon-ball_6-stars.png" width={50} height={50} alt="Dragon ball à 6 étoiles" />
+            <Image
+              className="col-start-3 row-start-4 row-span-2"
+              src="/assets/images/dragon-ball_6-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 6 étoiles"
+            />
           )}
           {correctCount >= totalCount - 1 && (
-            <Image src="/assets/images/dragon-ball_6-stars.png" width={50} height={50} alt="Dragon ball à 6 étoiles" />
-            // <Image src="/assets/images/dragon-ball_7-stars.png" width={50} height={50} alt="Dragon ball à 7 étoiles" />
+            <Image
+              className="col-start-2 row-start-5 row-span-2"
+              src="/assets/images/dragon-ball_7-etoiles.png"
+              width={50}
+              height={50}
+              alt="Dragon ball à 6 étoiles"
+            />
           )}
         </div>
       )}
     </>
+  );
+}
+
+function EmptySlot({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "size-9 m-2 rounded-full bg-gradient-to-b from-gray-100 to-gray-300 border-2 border-gray-300",
+        className
+      )}
+    ></div>
   );
 }
