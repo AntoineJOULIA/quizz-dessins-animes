743d6064a0900a8bd522ba97b2b24f010a2788d4
AntoineJOULIA <antoine.joulia@gmail.com>
2025-03-11 13:43:40 +0100

    chore(images): fix typos in file names

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Anime } from "@/types";
 // import rawData from "../data/dessins-animes.json";
-import rawData from "../data/dessins-animes_sample10.json";
-// import rawData from "../data/dessins-animes_sample.json";
+// import rawData from "../data/dessins-animes_sample10.json";
+import rawData from "../data/dessins-animes_sample.json";
 
 const data = rawData as Anime[];
 
