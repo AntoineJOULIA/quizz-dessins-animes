3383038c5302a1eceab79798c7d5c5ee870133de
AntoineJOULIA <antoine.joulia@gmail.com>
2025-03-14 08:44:00 +0100

    ui(navbar): change color on hover

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,13 +6,13 @@ export function Header() {
       <nav>
         <ul className="flex gap-8 justify-center items-center">
           <li>
-            <Link className="text-3xl p-4" href="/">
+            <Link className="text-3xl p-4 hover:text-primary" href="/">
               Accueil
             </Link>
           </li>
           <li>
-            <Link className="text-3xl p-4" href="/board">
-              Score
+            <Link className="text-3xl p-4 hover:text-primary" href="/board">
+              Progression
             </Link>
           </li>
         </ul>
