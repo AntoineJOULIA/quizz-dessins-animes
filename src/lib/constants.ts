46febbfc35ae1a7e84c43330fa22223e8e51cbf4
AntoineJOULIA <antoine.joulia@gmail.com>
2025-03-14 11:58:03 +0100

    chore(sanctuary): Taurus and Pisces thresholds different if "production"

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { House } from "@/hooks/useSanctuary";
 
 export const SANCTUARY_HOUSES_THRESHOLDS: { [Property in House]: number } = {
   belier: 0,
-  taureau: 2,
+  taureau: process.env.NODE_ENV === "production" ? 5 : 2,
   gemeaux: 0.1,
   cancer: 0.2,
   lion: 0.3,
@@ -12,7 +12,7 @@ export const SANCTUARY_HOUSES_THRESHOLDS: { [Property in House]: number } = {
   sagittaire: 0.7,
   capricorne: 0.8,
   verseau: 0.9,
-  poissons: 5,
+  poissons: process.env.NODE_ENV === "production" ? 5 : 2,
   pope: 1,
 };
 
