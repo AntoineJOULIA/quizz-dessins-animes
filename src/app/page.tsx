450fb41349024cb4a699fd4cf7ac0c350448908b
AntoineJOULIA <antoine.joulia@gmail.com>
2025-03-18 10:07:53 +0100

    ui(responsive): homepage illustration for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,40 @@ import { ResumeGameButton } from "@/components/resume-game-button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center mx-auto grow">
+    <div className="flex flex-col items-center mx-auto grow gap-4 md:gap-0">
       <Image
+        className="p-4 md:hidden"
+        src={imagePrefix() + "assets/homepage/homepage_illu-mobile.png"}
+        width={500}
+        height={343}
+        alt="Grand Quizz des dessins animés de notre enfance"
+      />
+      <Image
+        className="hidden md:block"
         src={imagePrefix() + "assets/homepage/homepage_illu.png"}
         width={1000}
         height={686}
         alt="Grand Quizz des dessins animés de notre enfance"
       />
-      <div className="flex flex-col md:flex-row gap-4">
+      <div className="flex flex-col md:flex-row gap-4 z-10">
         <NewGameButton />
         <ResumeGameButton />
       </div>
+
+      <Image
+        className="absolute bottom-0 left-0 max-w-[50vw] md:hidden"
+        src={imagePrefix() + "assets/homepage/homepage_illu-mobile-left.png"}
+        width={500}
+        height={343}
+        alt="Goldorak"
+      />
+      <Image
+        className="absolute bottom-0 right-0 max-w-[40vw] md:hidden"
+        src={imagePrefix() + "assets/homepage/homepage_illu-mobile-right.png"}
+        width={500}
+        height={343}
+        alt="Albator"
+      />
     </div>
   );
 }
